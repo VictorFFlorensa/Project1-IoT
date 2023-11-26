7f594eaa03b72547b878f8f38db8d70714d64459
LembranT <lembrant32@gmail.com>
2023-11-26 13:24:12 +0100

    light-bulb working

--- Actuators/light_bulb/light_actuator.py
+++ Actuators/light_bulb/light_actuator.py
@@ -3,26 +3,33 @@ import os
 from time import sleep
 import json
 
+host = os.environ.get("mqtt")
+
 # Espera 10 segundos para dar tiempo a que Kafka se inicie
 print("Esperando a que Kafka se inicie...")
 sleep(10)
-host = os.environ.get("mqtt")
+
+light_bulb_state = 0
 
 def on_message_actuate_light_bulb(client, userdata, message):
-    light_bulb_state = 0
+    global light_bulb_state
     payload = message.payload.decode('utf-8')
     data = json.loads(payload)
     presence_value = data.get('presence')
 
     if presence_value is not None:
-        if presence_value > 50:
+        if presence_value > 50 and light_bulb_state == 0:
             light_bulb_state = 1
-        elif presence_value < -50:
-            light_bulb_state = 0
+            print(f"Light Bulb state: {'ON' if light_bulb_state else 'OFF'}, because presence_value: {presence_value}")
 
-    print(f"Light Bulb state: {'ON' if light_bulb_state else 'OFF'}")
+        elif presence_value <= 50 and light_bulb_state == 1:
+            light_bulb_state = 0
+            print(f"Light Bulb state: {'ON' if light_bulb_state else 'OFF'}, because presence_value: {presence_value}")
+    else:
+        raise ValueError("Received presence value is None. Cannot process.")
 
 print("Starting...")
 while True:
-    subscribe.callback(on_message_actuate_light_bulb, "+", hostname=host)
+    topic = "light-bulb"
+    subscribe.callback(on_message_actuate_light_bulb, topic, hostname=host)
     sleep(1)
