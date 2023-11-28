bb2c6e78c3fe6754aece2132bec99012c9e98d24
Elena Barrachina i Ribes <ebarrachina2.olivera@gmail.com>
2023-11-28 11:30:16 +0100

    Added Tiffany and Tomy to dockerfile. Modified gateway to implement MQTT comunnication authentication.
    TODO: Fix authentication.

--- Gateways/gateway.py
+++ Gateways/gateway.py
@@ -4,15 +4,19 @@ from json import dumps
 from kafka import KafkaProducer
 import signal
 import sys
-import paho.mqtt.subscribe as subscribe
+import paho.mqtt.client as mqtt
 import os
+
 host = os.environ.get("mqtt")
 name = os.environ.get("user")
 kafka_url = os.environ.get("DOCKER_KAFKA_INIT_TOKEN")
+mqtt_username = "user1"
+mqtt_password = "password1"
 
 # Manejar finalización del programa
 def on_exit(signum, frame):
     print("Programa detenido manualmente.")
+    client.disconnect()
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, on_exit)
@@ -23,6 +27,10 @@ sleep(10)
 
 producer = KafkaProducer(bootstrap_servers=[kafka_url], value_serializer=lambda x: dumps(x).encode('utf-8'))
 
+def on_connect(client : mqtt.Client, userdata, flags, rc):
+     print(f"Conectado con código {rc}")
+     client.subscribe([("temperature-sensor", 0), ("presence-sensor", 0)])
+
 def on_message_print(client, userdata, message):
     payload = message.payload.decode('utf-8')
     data = json.loads(payload)
@@ -30,11 +38,14 @@ def on_message_print(client, userdata, message):
     data['is_cleaned'] = False
     topic = "raw_data"
     producer.send(topic, value=data)
-    print("Enviado por %s %s" % ("raw_data", data))
+    print(f"Enviado por {topic} {data}")
 
+client = mqtt.Client()
+client.username_pw_set(mqtt_username, password = mqtt_password)
+client.on_connect = on_connect
+client.on_message = on_message_print
 
 print("Starting...")
-subscribe.callback(on_message_print, ["temperature-sensor", "presence-sensor"], hostname=host)
 
-while True:
-        sleep(1)
+client.connect(host, 1883, 60)
+client.loop_forever()
